1e7f04e8243a3a1cb7285231f3a6b1f7de92c303
willian <wb.willian1300@gmail.com>
2025-06-27 15:17:14 -0300

    estrutura de repetição para converter mais temperaturas, DoWhile

--- src/temperatura/Main.java
+++ src/temperatura/Main.java
@@ -10,7 +10,10 @@ public class Main {
 		 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		 
+		
+		String continuar;
+		
+		do {
 		 System.out.println("DIgite o numero corepondente a convresão desejada:");
 	     System.out.println("1----Celsius para Fahrenheit ");
 	     System.out.println("2----Fahrenheit para Celsius");
@@ -22,22 +25,38 @@ public class Main {
 	     double result;
 	     
 	     switch (option) {
+	     
 	     case 1:
 	     System.out.println("Digite a temperatura ");
 	     inputTemp = sc.nextDouble();
 	     result = TemperatureConverter.CelsiusFahrenheit(inputTemp);
-	     System.out.printf("a temperatuar em Fahrenheit e: %.1f%n ", result);
+	     System.out.printf("A temperatuar em Fahrenheit e: %.1f%n ", result);
 		 break;
 		 
+	     case 2:
+		     System.out.println("Digite a temperatura ");
+		     inputTemp = sc.nextDouble();
+		     result = TemperatureConverter.FahrenheitCelsius(inputTemp);
+		     System.out.printf("A temperatuar em Celsius e: %.1f%n ", result);
+			 break;
+			 
+	     case 3:
+		     System.out.println("Digite a temperatura ");
+		     inputTemp = sc.nextDouble();
+		     result = TemperatureConverter.CelsiusKelvin(inputTemp);
+		     System.out.printf("A temperatuar em Kelvin e: %.1f%n ", result);
+			 break;
+		 
 		 default:
-			 System.out.println("opção invalida");
+			 System.out.println("Opção invalida");
 	     }
-		 
-		 
-		 
-		 
+	     System.out.println("Deseja continuar? SIM/NÃO?");
+		 continuar = sc.next();
+		} while (continuar.equalsIgnoreCase("SIm"));
+		
+		  System.out.println("Até a próxima");
 		 
 		 sc.close();	
- }
+     }
 }
 
